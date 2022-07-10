c04f2ebd43581de83f8c857a7e5d3175f9053092
Tross Yvormes <tross@pleaseignore.com>
2022-07-10 17:48:08 -0400

    Add import task

---
+++ timers2/import_esi.py
@@ -0,0 +1,103 @@
+import os
+import json
+from datetime import timezone
+
+import boto3
+import httpx
+from dateutil.parser import isoparse
+from boto3.dynamodb.conditions import Key
+
+from timers2.utils import get_timers, get_system_names, put_timer
+
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.environ["TABLE_NAME"])
+
+EVENT_TYPE_MAP = {
+    "station_defense": "Station",
+    "ihub_defense": "I-hub",
+    "tcu_defense": "TCU",
+}
+
+
+def handler(event, context):
+    response = httpx.get("https://esi.evetech.net/v1/sovereignty/campaigns")
+    response.raise_for_status()
+
+    data = response.json()
+
+    existing_campaigns = {
+        esi_campaign_id
+        for timer in get_timers(table)
+        if (esi_campaign_id := timer.get("esi_campaign_id"))
+    }
+
+    alliance_tickers = {}
+    alliances = {
+        standing["SK"][9:]: standing["standing_type"]
+        for standing in table.query(
+            KeyConditionExpression=(
+                Key("PK").eq("STANDING") & Key("SK").begins_with("ALLIANCE#")
+            )
+        )["Items"]
+    }
+
+    with httpx.Client() as client:
+        for item in data:
+            if item["campaign_id"] in existing_campaigns:
+                continue
+
+            if item["event_type"] not in EVENT_TYPE_MAP:
+                continue
+
+            if item["defender_id"] in alliance_tickers:
+                alliance_ticker = alliance_tickers[item["defender_id"]]
+            else:
+                try:
+                    alliance_response = client.get(
+                        "https://esi.evetech.net/v4/alliances/{}".format(
+                            item["defender_id"]
+                        )
+                    )
+                    alliance_response.raise_for_status()
+                    alliance_ticker = alliance_response.json().get("ticker")
+                    alliance_tickers[item["defender_id"]] = alliance_ticker
+                except (ValueError, httpx.HTTPStatusError, json.JSONDecodeError):
+                    continue
+
+            if alliance_ticker not in alliances:
+                continue
+
+            try:
+                system_name, region_name = get_system_names(
+                    client, item["solar_system_id"]
+                )
+            except (ValueError, httpx.HTTPStatusError, json.JSONDecodeError):
+                continue
+
+            if not alliance_ticker:
+                continue
+
+            start_time = isoparse(item["start_time"])
+            if not (
+                start_time.tzinfo is None and start_time.tzinfo.utcoffset(start_time)
+            ):
+                start_time = start_time.replace(tzinfo=timezone.utc)
+            else:
+                start_time = start_time.astimezone(timezone.utc)
+
+            put_timer(
+                table,
+                start_time=start_time,
+                system_name=system_name,
+                region_name=region_name,
+                corporation_ticker="AUTO",
+                alliance_ticker=alliance_ticker,
+                standing_type=alliances[alliance_ticker],
+                structure_type=EVENT_TYPE_MAP.get(item["event_type"], "Unknown"),
+                timer_type="Unknown",
+                replace="Not applicable",
+                notes="",
+                added_by="ESI",
+                esi_campaign_id=item["campaign_id"],
+            )
---
+++ timers2/utils.py
@@ -0,0 +1,115 @@
+import random
+import string
+from datetime import datetime, timezone, timedelta
+
+from dateutil.parser import isoparse
+from boto3.dynamodb.conditions import Key, Attr
+
+
+def get_system_names(client, system_id):
+    system_response = client.get(
+        "https://esi.evetech.net/v4/universe/systems/{}".format(system_id)
+    )
+    system_response.raise_for_status()
+    system_json = system_response.json()
+    system_name = system_json.get("name")
+    constellation_id = system_json.get("constellation_id")
+    if not system_name or not constellation_id:
+        raise ValueError()
+
+    constellation_response = client.get(
+        "https://esi.evetech.net/v1/universe/constellations/{}".format(constellation_id)
+    )
+    constellation_response.raise_for_status()
+    region_id = constellation_response.json().get("region_id")
+    if not region_id:
+        raise ValueError()
+
+    region_response = client.get(
+        "https://esi.evetech.net/v1/universe/regions/{}".format(region_id)
+    )
+    region_response.raise_for_status()
+    region_name = region_response.json().get("name")
+    if not region_name:
+        raise ValueError()
+
+    return system_name, region_name
+
+
+def get_timer_suffix():
+    return "".join(random.sample(string.ascii_letters + string.digits, 10))
+
+
+def put_timer(
+    table,
+    *,
+    start_time,
+    system_name,
+    region_name,
+    corporation_ticker,
+    alliance_ticker,
+    standing_type,
+    structure_type,
+    timer_type,
+    replace,
+    notes,
+    added_by,
+    esi_campaign_id=None
+):
+    item = {
+        "PK": "TIMER",
+        "SK": "TIMER#{}#{}".format(
+            start_time.isoformat(timespec="seconds"),
+            get_timer_suffix(),
+        ),
+        "TTL": int((start_time + timedelta(hours=24)).timestamp()),
+        "system_name": system_name,
+        "region_name": region_name,
+        "corporation_ticker": corporation_ticker,
+        "alliance_ticker": alliance_ticker,
+        "standing_type": standing_type,
+        "structure_type": structure_type,
+        "timer_type": timer_type,
+        "replace": replace,
+        "notes": notes,
+        "added_by": added_by,
+    }
+
+    if esi_campaign_id:
+        item["esi_campaign_id"] = esi_campaign_id
+
+    table.put_item(Item=item, ConditionExpression=Attr("PK").not_exists())
+
+
+def get_timers(table, only_active=True):
+    timers = []
+
+    now = datetime.now(tz=timezone.utc)
+
+    exclusive_start_key = None
+    while True:
+        kwargs = {
+            "KeyConditionExpression": Key("PK").eq("TIMER"),
+            "Limit": 50,
+        }
+        if exclusive_start_key:
+            kwargs["ExclusiveStartKey"] = exclusive_start_key
+
+        response = table.query(**kwargs)
+
+        for timer in response["Items"]:
+            sk_parts = timer["SK"].split("#")
+            if len(sk_parts) != 3:
+                continue
+
+            timer["start_time"] = isoparse(sk_parts[1])
+            if only_active and timer["start_time"] < now - timedelta(hours=1):
+                continue
+
+            timers.append(timer)
+
+        exclusive_start_key = response.get("LastEvaluatedKey")
+        if not exclusive_start_key:
+            break
+
+    return timers
