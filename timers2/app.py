2058255cfe06d416487800f8f7ba3b10a8f308dc
Tross Yvormes <tross@pleaseignore.com>
2022-07-10 18:08:58 -0400

    Add add timer form

--- timers2/app.py
+++ timers2/app.py
@@ -1,16 +1,27 @@
 import os
 
 import boto3
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from markupsafe import Markup
 
-from .utils import get_timers
+from .forms import TimerForm
+from .utils import get_timers, put_timer
 
 
 app = Flask(__name__)
+app.secret_key = os.environ["SECRET_KEY"]
 
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(os.environ["TABLE_NAME"])
+
+
+@app.template_filter()
+def field_errors(field):
+    return Markup("\n").join(
+        Markup('<div class="invalid-feedback">{}</div>').format(error)
+        for error in field.errors
+    )
 
 
 @app.route("/")
@@ -20,6 +31,34 @@ def index():
     return render_template(
         "index.html", user={"permissions": {"delete_timer": True}}, timers=timers
     )
+
+
+@app.route("/add-timer", methods=["GET", "POST"])
+def add_timer():
+    user = {"primary_character_name": "Tross Yvormes"}
+
+    form = TimerForm()
+    if form.validate_on_submit():
+        system_name, region_name = form.data["system"]
+
+        put_timer(
+            table,
+            start_time=form.data["start_time"],
+            system_name=system_name,
+            region_name=region_name,
+            corporation_ticker=form.data["corporation_ticker"],
+            alliance_ticker=form.data["alliance_ticker"],
+            standing_type=form.data["standing_type"],
+            structure_type=form.data["structure_type"],
+            timer_type=form.data["timer_type"],
+            replace=form.data["replace"],
+            notes=form.data["notes"],
+            added_by=user["primary_character_name"],
+        )
+
+        return redirect(url_for("index"))
+
+    return render_template("add_timer.html", form=form)
 
 
 @app.errorhandler(404)
---
+++ timers2/forms.py
@@ -0,0 +1,144 @@
+import re
+from datetime import datetime, timezone, timedelta
+
+import httpx
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, validators
+
+from .utils import get_system_names
+
+
+class DatetimeField(StringField):
+    def process_formdata(self, valuelist):
+        super().process_formdata(valuelist)
+
+        if "Reinforced until " in self.data:
+            time_start_index = self.data.index("Reinforced until ") + 17
+            datestr = self.data[time_start_index : time_start_index + 19]
+            self.data = datetime.strptime(datestr, "%Y.%m.%d %H:%M:%S")
+            return
+
+        timer_matches = list(
+            re.finditer(r"(?P<time_amt>[0-9]+)[ ]*(?P<dhms>[dhms]{1})", self.data)
+        )
+
+        if not timer_matches:
+            self.data = None
+
+        timer_time = datetime.now(tz=timezone.utc)
+        for this_match in timer_matches:
+            dhms = this_match.group("dhms")
+            delta_amt = int(this_match.group("time_amt"))
+            if dhms == "d":
+                timer_time = timer_time + timedelta(days=delta_amt)
+            elif dhms == "h":
+                timer_time = timer_time + timedelta(hours=delta_amt)
+            elif dhms == "m":
+                timer_time = timer_time + timedelta(minutes=delta_amt)
+            elif dhms == "s":
+                timer_time = timer_time + timedelta(seconds=delta_amt)
+
+        self.data = timer_time
+
+    def _value(self):
+        return (
+            str(self.raw_data[0])
+            if self.raw_data and len(self.raw_data) > 0 and self.raw_data[0] is not None
+            else ""
+        )
+
+
+class SystemField(StringField):
+    def process_formdata(self, valuelist):
+        super().process_formdata(valuelist)
+
+        with httpx.Client() as client:
+            search_response = client.get(
+                "https://esi.evetech.net/v2/search/",
+                params={
+                    "search": self.data,
+                    "categories": "solar_system",
+                    "strict": "true",
+                },
+            )
+            search_response.raise_for_status()
+
+            search_data = search_response.json()
+            system_id = search_data and search_data.get("solar_system")
+            if system_id:
+                system_id = system_id[0]
+
+            if not system_id:
+                self.data = None
+
+            self.data = get_system_names(client, system_id)
+
+    def _value(self):
+        return (
+            str(self.raw_data[0])
+            if self.raw_data and len(self.raw_data) > 0 and self.raw_data[0] is not None
+            else ""
+        )
+
+
+class TimerForm(FlaskForm):
+    start_time = DatetimeField(
+        "Date/time",
+        validators=[validators.DataRequired()],
+    )
+    system = SystemField(
+        "System",
+        validators=[validators.DataRequired(message="Invalid solar system")],
+    )
+    corporation_ticker = StringField(
+        "Corporation ticker", validators=[validators.InputRequired()]
+    )
+    alliance_ticker = StringField("Alliance ticker")
+    standing_type = SelectField(
+        "Standing",
+        choices=["Friendly", "Hostile", "It's complicated", "Unknown"],
+        validators=[validators.InputRequired()],
+    )
+    structure_type = SelectField(
+        "Structure type",
+        choices=[
+            "I-hub",
+            "PoCo",
+            "Tower",
+            "TCU",
+            "Other/Unknown",
+            "Athanor",
+            "Astrahaus",
+            "Raitaru",
+            "Azbel",
+            "Fortizar",
+            "Tatara",
+            "Sotiyo",
+            "Keepstar",
+            "Ansiblex",
+        ],
+        validators=[validators.InputRequired()],
+    )
+    timer_type = SelectField(
+        "Timer type",
+        choices=[
+            "Shield",
+            "Armor",
+            "Anchoring",
+            "Structure",
+            "Armor + Structure",
+            "Not Applicable",
+            "Unknown",
+        ],
+        validators=[validators.InputRequired()],
+    )
+    replace = SelectField(
+        "Replace",
+        choices=[
+            "Not Applicable",
+            "Logistics Replacement",
+            "Corp Replacement",
+        ],
+        validators=[validators.InputRequired()],
+    )
+    notes = TextAreaField("Notes")
